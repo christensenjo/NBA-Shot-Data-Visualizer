cd51dd5aedfa34cfbf89aaa3c1a3be77b67e7663
Joel Christensen <christensenjo14@gmail.com>
2022-11-05 16:49:15 -0600

    Add .idea to gitignore and continue work on testing rpy2

--- data_api/teamShots_rpy2.py
+++ data_api/teamShots_rpy2.py
@@ -2,6 +2,11 @@
 ######  Users must first install rpy2 via:
 ######  <pip install rpy2>
 ######
+######  and change the R home directory below...
+######
+import os
+os.environ["R_HOME"] = r"C:\Program Files\R\R-4.1.2" # change as needed
+
 import rpy2
 
 from rpy2.robjects.packages import importr
@@ -19,13 +24,18 @@ names_to_install = [x for x in packnames if not rpackages.isinstalled(x)]
 if len(names_to_install) > 0:
     utils.install_packages(StrVector(names_to_install))
 
-result = rpy2.robjects.r('''
+result = rpy2.robjects.r('''return 50 * 2''')
 
-    # Write R code here
 
-    return 50 * 2
-    
-    
-''')
+# get_ipython().run_cell_magic(
+# "R",
+# "-i df",
+# '''
+# library(ggplot2)
+# ggplot(df, aes(x=reorder(cups_of_coffee,productivity), y=productivity)) +
+# geom_col() +
+# coord_flip()
+# '''
+# )
 
 print(result)
