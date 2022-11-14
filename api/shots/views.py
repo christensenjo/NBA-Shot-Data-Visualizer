ba06562d24dbfc97d886ac3428e2ee4002e0054c
Marshal <mptaylor777@gmail.com>
2022-11-13 17:49:39 -0700

    Format method on JSON response

--- api/shots/views.py
+++ api/shots/views.py
@@ -1,4 +1,5 @@
 from django.core import serializers
+from django.db.models import F
 from django.shortcuts import render
 import json
 from django.http import HttpResponse, JsonResponse
@@ -17,10 +18,29 @@ def get(request):
     start_date = request.GET.get('start_date',None)
     end_date = request.GET.get('end_date',None)
     player_name = request.GET.get('player',None)
+    json_format = request.GET.get('format', "default")
     shots = Shot.objects.filter(game__date__gte=start_date, game__date__lte=end_date, player__name=player_name)
-    #return JsonResponse(serializers.serialize('json', shots), safe=False)
-    # return JsonResponse(list(shots.values()), safe=False)
-    return JsonResponse(ShotSerializer(shots, many=True).data, safe=False)
+
+    schema = ('id', 'period', 'minutes_remaining', 'location_x', 'location_y', 'seconds_remaining', 'distance', 'is_attempted', 'is_made')
+    k_schema = {
+        "player_name": F("player__name"),
+        "team": F("player__team__name"),
+        "shot_action": F("action__name"),
+        "shot_event": F("event__name"),
+        "shot_type_name": F("shot_type__name"),
+        "shot_zone": F("zone__description"),
+        "zone_type": F("zone__zone_type__name"),
+        "game_date": F("game__date"),
+    }
+
+    data = {}
+    if json_format == "nested":
+        data = ShotSerializer(shots, many=True).data
+    elif json_format == "schema":
+        data = list(shots.values(*schema, **k_schema))
+    else:
+        data = list(shots.values())
+    return JsonResponse(data, safe=False)
 
 
 def seed(request):
