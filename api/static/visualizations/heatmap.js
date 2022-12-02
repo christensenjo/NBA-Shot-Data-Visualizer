9ae0f4ce39fea2b8473d83ad107ffb76c65c7db8
Joel Christensen <christensenjo14@gmail.com>
2022-12-02 11:10:36 -0700

    Attempting to create grid of rectangles over svg with viewbox

--- api/static/visualizations/heatmap.js
+++ api/static/visualizations/heatmap.js
@@ -2,7 +2,8 @@ function heatMap(){
     // SVG Setup Details
     let margin = {"top": 20, "bottom": 0, "left": 60, "right": 20};
 
-    let width = document.getElementById("heatmap").offsetWidth - margin.left - margin.right;
+    let width = document.getElementById("heatmapPane").offsetWidth - margin.left - margin.right;
+    width = width - (.24 * width); // Account for padding on heatmap court svg
     let height = document.getElementById("heatmapPane").offsetHeight - margin.top - margin.bottom - document.getElementById("heatmapTitle").offsetHeight;
     let dataset = [];
 
@@ -21,11 +22,7 @@ function heatMap(){
     let axisScale = d3.scaleQuantile().range(fillRange);
 
     // SVG Implementation
-    // let heatmapSVG = d3.select("#heatmap").append("svg")
-    //     .attr("width", width + margin.left + margin.right)
-    //     .attr("height", height + margin.top + margin.bottom)
-    //     .append("g")
-    //     .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
+    let heatmapSVG = d3.select("#court");
 
     // Implement with Data
     // console.log(data);
@@ -41,7 +38,7 @@ function heatMap(){
 
         let legendAxis = d3.axisBottom(axisScale).tickFormat(x=>  x.toFixed(1) + "%");
 
-        let legend = svg.selectAll(".legend")
+        let legend = heatmapSVG.selectAll(".legend")
             .data(colors)
             .enter().append("g")
             .attr("transform", "translate(" + (width / 2 - legendWidth / 2) + ", " + height + ")");
@@ -52,26 +49,33 @@ function heatMap(){
             .style("fill", d=>d)
             .attr("x", (d,i)=> legendWidth/colors.length * i);
 
-        svg.append("g").attr("class", "axis")
+        heatmapSVG.append("g").attr("class", "axis")
             .attr("transform", "translate(" + (width / 2 - legendWidth/2) + ", " + (height - legendHeight + 60) + ")")
             .call(legendAxis);
     }
 
-    function initCourt(){
-        // Visualize the court
-
+    function init(){
         // NBA Court Dimension Setup (Halfcourt only)
         let courtWidth = 50;
         let courtHeight = 40;
 
+        let courtH = document.getElementById("heatmap").offsetHeight;
+        console.log(courtH);
+        // Make heatmap grid
+        for(let i = 0; i < 10; i++){
+            for(let j = 0; j < 10; j++){
+                console.log("height is " + height);
+                console.log("rect h is " + (.10 * height));
 
-        //Free Throw
-
-        //Other
-
-
+                heatmapSVG.append("rect")
+                    .attr("width", .10 * width)
+                    .attr("height", .10 * height)
+                    .style("fill", "red")
+                    .attr("x", ((i/10) * width))
+                    .attr("y", ((j/10) * height));
+            }
+        }
     }
 
-    initCourt();
-
+    init();
 }
