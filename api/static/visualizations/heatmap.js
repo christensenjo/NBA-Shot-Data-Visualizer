47127bfa20fab0c8bd3a893ad371b77153c9b90f
Joel Christensen <christensenjo14@gmail.com>
2022-12-03 17:38:46 -0700

    Heatmap square correctly generating via data, but doesn't update properly

--- api/static/visualizations/heatmap.js
+++ api/static/visualizations/heatmap.js
@@ -1,10 +1,8 @@
 function heatMap(data){
     // SVG Setup Details
     let margin = {"top": 20, "bottom": 0, "left": 60, "right": 20};
-
-    let width = document.getElementById("heatmapPane").offsetWidth - margin.left - margin.right;
-    let height = document.getElementById("heatmapPane").offsetHeight - margin.top - margin.bottom - document.getElementById("heatmapTitle").offsetHeight;
-    let dataset = [];
+    let width = 700;
+    let height = 559;
 
     // Color Scaling Details
     let colors = ["#80000F", "#BF0071", "#FF05FF", "#BC44FF",
@@ -12,19 +10,17 @@ function heatMap(data){
     let fillRange = [];
     let legendWidth = width / 5;
     let legendHeight = 30;
-
+    let max;
+    let min;
     for(let i = 0; i <= colors.length; i++){
         fillRange.push(legendWidth/colors.length * i);
     }
-
     let fill = d3.scaleQuantile().range(colors);
     let axisScale = d3.scaleQuantile().range(fillRange);
 
     // SVG Implementation
-    let heatmapSVG = d3.select("#court").append("g");
-
-    // Implement with Data
-    // console.log(data);
+    let heatmapSVG = d3.select("#court")
+        .append("g");
 
     function createLegend(max, min){
         let diff = (max - min)/colors.length;
@@ -55,15 +51,31 @@ function heatMap(data){
 
     function init(data){
         //Data Transformation and Handling
+        // console.log("WIDTH is " + width);
+        // console.log("HEIGHT is " + height);
         let shotGrid = [];
         for(let i = 0; i < 10; i++){
             shotGrid[i] = new Array(10);
+
+            for(let j = 0; j < 10; j++){
+                shotGrid[i][j] = {"x": i, "y": j};
+
+                //Create initial grid rects
+                // heatmapSVG.append("rect")
+                // .attr("width", .10 * width)
+                // .attr("height", .10 * height)
+                // .style("fill", "none")
+                // .attr("x", ((i/10) * width))
+                // .attr("y", ((j/10) * height))
+                // .style("opacity", "0%")
+                // .attr("class", "heatRect");
+
+            }
         }
         console.log("empty shotGrid init:")
         console.log(shotGrid);
-        console.log((20/25) / 0.2)
 
-        count = 0;
+        let count = 0;
         data.forEach(function(d){
             let x = d['location_x'];
             let xGrid = getXGrid(x);
@@ -72,17 +84,37 @@ function heatMap(data){
             let yGrid = getYGrid(y);
 
             let made = d['is_made'];
-            // TODO: handle dict instantiation prior to update
-            let currentAttempts = shotGrid[xGrid][yGrid]['attempts'];
-            let currentMade = shotGrid[xGrid][yGrid]['made'];
-            if(made){
-                shotGrid[xGrid][yGrid] = {'attempts': currentAttempts + 1,
-                                            'made': currentMade + 1,
-                                            'fg%': ((currentMade + 1)/(currentAttempts + 1)) };
+            let currentAttempts;
+            // console.log(xGrid);
+            // console.log("y is " + (y/ 10));
+            // console.log(yGrid);
+            // console.log(shotGrid);
+            if('attempts' in shotGrid[xGrid][yGrid]){
+                currentAttempts = shotGrid[xGrid][yGrid]['attempts'];
             }else{
-                shotGrid[xGrid][yGrid] = {'attempts': currentAttempts + 1,
-                                            'made': currentMade,
-                                            'fg%': (currentMade/(currentAttempts + 1)) };
+                currentAttempts = 0;
+            }
+            let currentMade;
+            if('made' in shotGrid[xGrid][yGrid]){
+                currentMade = shotGrid[xGrid][yGrid]['made'];
+            }else{
+                currentMade = 0;
+            }
+            let newFG = ((currentMade + 1)/(currentAttempts + 1));
+            if(newFG > max){
+                max = newFG;
+            }
+            if(newFG < min){
+                min = newFG;
+            }
+            if(made){
+                shotGrid[xGrid][yGrid]['attempts'] = currentAttempts + 1;
+                shotGrid[xGrid][yGrid]['made']  = currentMade + 1;
+                shotGrid[xGrid][yGrid]['fg%'] = newFG;
+            }else{
+                shotGrid[xGrid][yGrid]['attempts'] = currentAttempts + 1;
+                shotGrid[xGrid][yGrid]['made']  = currentMade;
+                shotGrid[xGrid][yGrid]['fg%'] = newFG;
             }
 
             //printing for debug purposes
@@ -93,15 +125,6 @@ function heatMap(data){
            }
         });
 
-        // TODO: remove
-        for(let i = 0; i < 10; i++){
-            for(let j = 0; j < 10; j++){
-
-            }
-        }
-
-
-        // TODO: use join-update to create grid based on data
         updateHeatMap(shotGrid);
 
     }
@@ -109,26 +132,28 @@ function heatMap(data){
     function getXGrid(x){
         x = x / 250;
 
-        if(x < -0.8){
+        if(x <= -0.8){
+            return 0;
+        }else if(x <= -0.6 && x > -0.8){
             return 1;
-        }else if(x < -0.6 && x > -0.8){
+        }else if(x <= -0.4 && x > -0.6){
             return 2;
-        }else if(x < -0.4 && x > -0.6){
+        }else if(x <= -0.2 && x > -0.4){
             return 3;
-        }else if(x < -0.2 && x > -0.4){
+        }else if(x <= 0 && x > -0.2){
             return 4;
-        }else if(x < 0 && x > -0.2){
+        }else if(x <= 0.2 && x > 0){
             return 5;
-        }else if(x < 0.2 && x > 0){
+        }else if(x <= 0.4 && x > 0.2){
             return 6;
-        }else if(x < 0.4 && x > 0.2){
+        }else if(x <= 0.6 && x > 0.4){
             return 7;
-        }else if(x < 0.6 && x > 0.4){
+        }else if(x <= 0.8 && x > 0.6){
             return 8;
-        }else if(x < 0.8 && x > 0.6){
+        }else if(x <= 1 && x > 0.8){
             return 9;
-        }else if(x < 1 && x > 0.8){
-            return 10;
+        }else{
+            console.log(x);
         }
     }
 
@@ -136,62 +161,65 @@ function heatMap(data){
         y = y / 10;
         // grid positions from left-top corner
         // start from higher distances
-        if(y > 35.475){
-            yGrid = 1;
-        }else if(y > 30.95 && y < 35.475){
-            yGrid = 2;
-        }else if(y > 26.425 && y < 30.95){
-            yGrid = 3;
-        }else if(y > 21.9 && y < 26.425){
-            yGrid = 4;
-        }else if(y > 17.375 && y < 21.9){
-            yGrid = 5;
-        }else if(y > 12.85 && y < 17.375){
-            yGrid = 6;
-        }else if(y > 8.325 && y < 12.85){
-            yGrid = 7;
-        }else if(y > 3.8 && y < 8.325){
-            yGrid = 8;
-        }else if(y > -0.725 && y < 3.8){
-            yGrid = 9;
+        if(y >= 35.475){
+            return 0;
+        }else if(y >= 30.95 && y < 35.475){
+            return 1;
+        }else if(y >= 26.425 && y < 30.95){
+            return 2;
+        }else if(y >= 21.9 && y < 26.425){
+            return 3;
+        }else if(y >= 17.375 && y < 21.9){
+            return 4;
+        }else if(y >= 12.85 && y < 17.375){
+            return 5;
+        }else if(y >= 8.325 && y < 12.85){
+            return 6;
+        }else if(y >= 3.8 && y < 8.325){
+            return 7;
+        }else if(y >= -0.725 && y < 3.8){
+            return 8;
         }else if(y < -0.725){
-            yGrid = 10;
+            return 9;
         }
     }
 
     function updateHeatMap(shotGrid){
-        // TODO: Get min and max rates from the shotGrid
-
         // Create the legend
         createLegend(max, min);
         fill.domain([min, max]);
 
         width = 700;
         height = 559;
+        gridSquares = [].concat(...shotGrid);
+        // console.log("HERE");
+        // console.log(gridSquares);
 
         heatmapSVG.selectAll(".heatRects")
-            .data(shotGrid)
+            .data(gridSquares)
             .join(
                 function (enter){
                     return enter.append("rect")
                         .attr("width", .10 * width)
                         .attr("height", .10 * height)
-                        .style("fill", function(d){
-                            // TODO: code initial scale fill?
-                            return "red";
+                        .style("fill", function(d, i){
+                            return fill(d['fg%']);
                         })
-                        .attr("x", ((i/10) * width))
-                        .attr("y", ((j/10) * height))
-                        .style("opacity", "0%")
-                        .attr("class", "heatRect");
+                        .style("opacity", "50%")
+                        .attr("class", "heatRect")
+                        .attr("y", function(d){
+                            return d['y']/10 * height;
+                        })
+                        .attr("x", function(d){
+                            return d['x']/10 * width;
+                        });
+
                 },
                 function (update){
                     return update
                         .style("opacity", "50%")
                         .style("fill", function(d){
-                            //TODO: code fill function using scale based on fg%
-                            // return fill(d['fg%']);
-                            return "red";
+                            return fill(d['fg%']);
                         })
                 },
                 function (exit){
