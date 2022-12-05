443fd383b9a59de03d4d720ed5f54614590b3020
Joel Christensen <christensenjo14@gmail.com>
2022-12-05 14:27:49 -0700

    Cleanup and remove getShots()

--- api/static/visualizations/dataFunctions.js
+++ api/static/visualizations/dataFunctions.js
@@ -1,15 +1,3 @@
-function getShots() {
-    fetch('http://' + window.location.host + '/shots/get/?player=' + document.getElementById('player').value + '&start_date=' + document.getElementById('start_date').value + '&end_date=' + document.getElementById('end_date').value)
-        .then(response => response.json())
-        .then(data => {
-            //Pane 1 - League Comparison
-
-            //Pane 2 - Heatmap
-            //heatMap(data)
-
-        });
-}
-
 let players = fetch('http://' + window.location.host + '/players/get')
     .then(response => response.json())
     .then(data => {
