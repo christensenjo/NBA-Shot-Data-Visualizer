87ea6bad58087cab1c64709527b1829a1b73839b
Marshal <mptaylor777@gmail.com>
2022-12-05 19:05:20 -0700

    3point line and labels

--- api/static/visualizations/ShotDistance.js
+++ api/static/visualizations/ShotDistance.js
@@ -27,6 +27,40 @@ export class ShotDistance extends D3Chart {
         this.axis.yAxis = this.svg.append("g")
             .attr("class", "yAxis")
             .call(this.axis.y)
+
+
+        let height = this.size.height
+        let width = this.size.width
+
+        this.svg.append("text")
+            .attr("class", "xlabel")
+            .attr("text-anchor", "middle")
+            .attr("x", width/2)
+            .attr("y", height + 32)
+            .text("Distance in Feet");
+
+        this.svg.append("text")
+            .attr("class", "ylabel")
+            .attr("text-anchor", "middle")
+            .attr("x", -height/2)
+            .attr("y", -32)
+            .attr("transform", "rotate(-90)")
+            .text("Count of Shots");
+
+        this.point3 = this.svg.append("text")
+            .attr("class", "point3-label")
+            .attr("text-anchor", "middle")
+            .attr("x", -height/2)
+            .attr("y", -32)
+            .attr("transform", "rotate(-90)")
+            .text("3 Point");
+
+
+        this.tooltip = d3.select(this.selector).append("div")
+            .style("position", "absolute")
+//            .style("visibility", "hidden")
+            .text("I'm a circle!")
+            .attr("class", "line-tooltip")
     }
 
     updateAxis = function() {
@@ -45,7 +79,7 @@ export class ShotDistance extends D3Chart {
         this.bins = histogram(this.data)
         let bins = this.bins
 
-        this.axis.yScale.domain([0, d3.max(bins, function(d) { return d.length; }) + 50])
+        this.axis.yScale.domain([0, d3.max(bins, function(d) { return d.length; }) + 10])
 
     }
 
@@ -62,22 +96,49 @@ export class ShotDistance extends D3Chart {
 
         let data = this.bins
 
+        let tooltip = this.tooltip
 
         this.svg.selectAll("rect").data(data)
             .join("rect")
+//            .transition().duration(1000)
             .attr("x", 1)
-            .attr("transform", function(d) { return "translate(" + xScale(d.x0) + "," + (yScale(d.length)) + ")"; })
-            .attr("width", function(d) { return xScale(d.x1) - xScale(d.x0) -1 ; })
-            .attr("height", function(d) { return height - yScale(d.length); })
+            .attr("transform", function(d) { return "translate(" + xScale(d.x0) + "," + (yScale(d.length-2)) + ")"; })
+            .attr("width", function(d) { return Math.max(xScale(d.x1) - xScale(d.x0),0); })
+            .attr("height", function(d) { return Math.max(height - yScale(d.length-2), 0); })
             .style("fill", "#198754")
-            .on("mouseover", function(d) {
-                d3.select(this)
-                    .style("fill", "#157347");
+            .attr("class", ".bar")
+            .attr('data-message', function(d){
+                return `Shots: ${(d.length)-2} <br> Distance: ${d.x0}`
             })
-            .on("mouseleave", function(d) {
-                d3.select(this)
-                    .style("fill", "#198754");
-            });
+            .on("mouseover", function(e) {
+                d3.select(this).style("fill", "#157347");
+                tooltip.style("display", "initial")
+                tooltip.html(e.target.getAttribute('data-message'))
+                let rect = e.target.getBoundingClientRect()
+                let tooltipRect = tooltip.node().getBoundingClientRect()
+                tooltip.style("top", (rect.top-35)+"px").style("left",(rect.left-(tooltipRect.width/2)+(rect.width/2))+"px")
+            })
+            .on("mouseleave", function(e) {
+                d3.select(this).style("fill", "#198754");
+                tooltip.style("display", "none")
+            })
+
+        let d = [{"distance":22}]
+
+        this.svg.selectAll(".point3").data(d)
+            .join("rect")
+            .attr("x", 1)
+            .attr("transform", function(d) { return "translate(" + xScale(d.distance) + "," + 0 + ")"; })
+            .attr("width", function(d) { return 1 })
+            .attr("height", function(d) { return height })
+            .attr("class", "point3")
+
+
+        this.svg.selectAll(".point3-label").data(d)
+            .attr("y", function(d){return xScale(d.distance)})
+            .attr("x",-25)
+            .attr("width", function(d) { return 1 })
+            .attr("height", function(d) { return height })
 
     }
 }
