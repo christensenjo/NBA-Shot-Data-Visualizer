f3c51d441465892850e2da451b6e49461ea4f677
Hagen Larsen <hagenjay7@outlook.com>
2022-12-05 18:59:10 -0700

    player data populated via endpoint

--- api/players/urls.py
+++ api/players/urls.py
@@ -5,4 +5,6 @@ from players import views
 urlpatterns = [
     path('get/', views.get, name='get'),
     path('seed/', views.seedPlayers, name='seed'),
+    path('seedData/', views.seedData, name='seedData'),
+    path('getPlayerData/<str:player>', views.getPlayerData, name='getPlayerData'),
 ]
--- api/players/views.py
+++ api/players/views.py
@@ -11,6 +11,21 @@ def get(request):
     players = df['namePlayer'].drop_duplicates()
     return HttpResponse(json.dumps({'players': players.tolist()}))
 
+def getPlayerData(request, player):
+    player = Player.objects.filter(name=player)
+    if player:
+        player = player[0]
+        playerData = [player.ppg, player.rpg, player.apg, player.topg, player.salary]
+    else:
+        playerData = [0, 0, 0, 0, 0]
+    
+    leagueData = []
+    players = Player.objects.all()
+    for p in players:
+        leagueData.append([p.ppg, p.rpg, p.apg, p.topg, p.salary])
+
+    return HttpResponse(json.dumps({'playerData': playerData, 'leagueData': leagueData}))
+        
 
 def seedPlayers(request):
     files = os.listdir('../data')
@@ -32,4 +47,35 @@ def seedPlayers(request):
 
     data['createdCount'] = len(data['created'])
     data['totalPlayers'] = Player.objects.all().count()
+    return HttpResponse(json.dumps(data))
+
+
+def seedData(request):
+    files = os.listdir('../data')
+    data = {'created': []}
+    for file in files:
+        if file.endswith('salaries.csv'):
+            df = pd.read_csv('../data/' + file)
+            for index, row in df.iterrows():
+                player = Player.objects.filter(name=row['Player'])
+                if player:
+                    for p in player:
+                        p.salary = float(row['2022-23'])
+                        p.save()
+                        data['created'].append({'name': p.name, 'salary': p.salary})
+
+        if file.endswith('stats.csv'):
+            df = pd.read_csv('../data/' + file)
+            for index, row in df.iterrows():
+                player = Player.objects.filter(name=row['Player'])
+                if player:
+                    for p in player:
+                        p.ppg = row['PTS']
+                        p.rpg = row['TRB']
+                        p.apg = row['AST']
+                        p.topg = row['TOV']
+                        p.save()
+                        data['created'].append({'name': p.name, 'ppg': p.ppg, 'rpg': p.rpg, 'apg': p.apg, 'topg': p.topg})
+
+    data['createdCount'] = len(data['created'])
     return HttpResponse(json.dumps(data))
